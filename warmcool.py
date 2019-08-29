6118c471801805d06c83b9c102c14ac4a7fc8d90
katewhittingham <54670401+katewhittingham@users.noreply.github.com>
2019-08-29 12:57:08 +0100

    Add files via upload

---
+++ warmcool.py
@@ -0,0 +1,43 @@
+import random
+
+n = random.randint(1,100)
+
+guess = int(input("Guess a number from 1 to 99: "))
+
+a = abs(guess - n)
+
+while a != 0:
+    if 15 < a < 50:
+        print("You are warm!")
+        guess = int(input("Guess again: "))
+        break
+    elif a < 16:
+        print("You are very warm!")
+        guess = int(input("Guess again: "))
+        break
+    else:
+        print("You are cold!")
+        guess = int(input("Guess again: "))
+        break
+else:
+    print("You guessed it!")
+
+    
+b = a
+
+a = abs(guess - n)
+
+
+while a != 0:
+    if a < b:
+        print("You are warmer!")
+    elif a > b:
+        print("You are cooler!")
+        
+    guess = int(input("Guess again: "))
+    b = a
+    a = abs(guess - n)
+else:
+    print("You guessed it!")
+
+    
